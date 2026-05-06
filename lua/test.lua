56b30cf96d32fdcec3733f921facf74cfeb645f4
theacebutler <theacebutler@gmail.com>
2026-05-05 20:43:16 -0400

    initiall commit

--- lua/test.lua
+++ lua/test.lua
@@ -1,9 +1,38 @@
--- open a new tmux window in the root of the project
-local function open_tmux_window()
-	local cmd = "tmux split-window -h"
-	os.execute(cmd)
+local M = {}
+
+local function getTestCommand(ft)
+	if ft == "go" then
+		return "go test  -v ./..."
+	elseif ft == "js" then
+		return "bun run test"
+	elseif ft == "ts" then
+		return "bun run test"
+	elseif ft == "tsx" then
+		return "bun run test"
+	end
+	vim.notify("No test command found for filetype " .. ft, vim.log.levels.ERROR)
+	return "echo 'No test command found for filetype " .. ft .. "'"
 end
 
-vim.api.nvim_create_user_command("OpenTmuxWindow", function()
-	open_tmux_window()
+M.open = function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local ft = vim.bo.filetype
+	vim.cmd("vsplit")
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(win, buf)
+	local c = getTestCommand(ft)
+	print(c)
+	vim.fn.jobstart(c, {
+		term = true,
+	})
+	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":q<CR>", { noremap = true, silent = true })
+	M.win = win
+end
+
+vim.keymap.set("n", "<leader>tt", ":Test<CR>", { noremap = true, silent = true })
+vim.api.nvim_create_user_command("Test", function()
+	if M.win and vim.api.nvim_win_is_valid(M.win) then
+		vim.api.nvim_win_close(M.win, true)
+	end
+	M.open()
 end, {})
